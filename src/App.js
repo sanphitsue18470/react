2b290e71949c5c45341a5e38fef362f1b892bf55
unknown <6110110463@psu.ac.th>
2020-08-16 00:26:53 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
 const word = "Hello";
 
 function App() {
  return (
  <div>
-     {
-        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
-     }
+    <WordCard value={word}/>
  </div>
  );
 }
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function CharacterCard(props){
     return (
-        <div>
+        <div className ="card">
             {props.value}
         </div>
     )
